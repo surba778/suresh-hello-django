cbcd82a5943802761291f83d252af3bb955fbb6a
surba778 <surba778@gmail.com>
2022-01-18 06:09:35 +0000

    Added Hostname

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = True
 
-if development:
-    ALLOWED_HOSTS = ['localhost']
-else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = ['soori-django-todo-app.herokuapp.com']
+
 
 
 # Application definition
